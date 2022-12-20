f6e097628dcf80ec9b1204a523efd4de82463569
sogbeye <soclassique@yahoo.com>
2022-12-20 23:10:58 +0000

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
+"""Define a square """
 class Square:
-
-    """Empty class that defines a square"""
-
-    pass
+"""Does nothing"""
+pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-
 """Defines a square """
-
 class Square:
-
-    """Represents a square"""
-
-    def __init__(self, size):
-
-        """Initializing this square class
-
-        Args: size - represnets the size of the square defined
-
-        """
-
-        self.__size = size
+"""Represents a square"""
+def __init__(self, size):
+"""Initializing this square class
+Args: size - represnets the size of the square defined
+"""
+self.__size = size
