3fc53c52eddaf9c980f722a4e03afd40fbd75192
sogbeye <soclassique@yahoo.com>
2022-12-20 23:14:03 +0000

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+
 """Defines a square """
+
 class Square:
-"""Represents a square"""
-def __init__(self, size):
-"""Initializing this square class
-Args: size - represnets the size of the square defined
-"""
-self.__size = size
+
+    """Represents a square"""
+
+    def __init__(self, size):
+
+        """Initializing this square class
+
+        Args: size - represnets the size of the square defined
+
+        """
+
+        self.__size = size
