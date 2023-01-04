8b3d6a36f763ae3f5bfe0a3bb5575a53aaa7ce35
sogbeye <soclassique@yahoo.com>
2023-01-04 00:18:44 +0000

    update

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-
 """locked class module"""
-
 class LockedClass:
-
-    """locking the __slots__"""
-
-    __slots__ = 'first_name'
+"""locking the __slots__"""
+__slots__ = 'first_name'
